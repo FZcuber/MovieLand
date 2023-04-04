e152f580e9a2a2ed3c32c75d3674eb6f452b5114
Frank Zhou <fzcuber@gmail.com>
2023-04-04 12:37:06 -0500

    updated UI

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -1,13 +1,48 @@
 import React from "react";
 
 const MovieCard = ({ movie }) => {
+  const styles = {
+    movie: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      width: "250px",
+      borderRadius: "10px",
+      boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
+      padding: "16px",
+      margin: "16px",
+      backgroundColor: "white",
+    },
+    moviePoster: {
+      width: "100%",
+      borderRadius: "5px",
+    },
+    movieTitle: {
+      fontSize: "1.2em",
+      fontWeight: "bold",
+      marginTop: "8px",
+      textAlign: "center",
+    },
+    movieYear: {
+      fontSize: "1em",
+      color: "#999",
+      marginBottom: "8px",
+    },
+    movieType: {
+      fontSize: "0.9em",
+      fontStyle: "italic",
+      marginBottom: "4px",
+    },
+  };
+
   return (
-    <div className="movie">
+    <div style={styles.movie}>
       <div>
-        <p>{movie.year}</p>
+        <p style={styles.movieYear}>{movie.year}</p>
       </div>
       <div>
         <img
+          style={styles.moviePoster}
           src={
             movie.Poster !== "N/A"
               ? movie.Poster
@@ -17,8 +52,8 @@ const MovieCard = ({ movie }) => {
         ></img>
       </div>
       <div>
-        <span>{movie.Type}</span>
-        <h3>{movie.Title}</h3>
+        <span style={styles.movieType}>{movie.Type}</span>
+        <h3 style={styles.movieTitle}>{movie.Title}</h3>
       </div>
     </div>
   );
