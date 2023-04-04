7a366b1825309763649fb6d56ac5e1f265ba5e66
Frank Zhou <fzcuber@gmail.com>
2023-04-04 11:36:58 -0500

    added docs

--- src/App.js
+++ src/App.js
@@ -5,12 +5,16 @@ import SearchIcon from "./search.svg";
 import MovieCard from "./MovieCard";
 import movieTitlesJson from "./movieTitles";
 
+// get movie titles from JSON file
 const movieTitles = movieTitlesJson.movies;
 
+// OMDB API key
 const api_key = "c3672265";
 
+// URL for OMDB API
 const API_URL = `https://www.omdbapi.com/?apikey=${api_key}&s=`;
 
+// function to shuffle an array
 const shuffleArray = (array) => {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
@@ -19,20 +23,31 @@ const shuffleArray = (array) => {
   return array;
 };
 
+// Main App component
 const APP = () => {
+  // search value state
   const [searchValue, setSearchValue] = useState("");
+
+  // movies state
   const [movies, setMovies] = useState([]);
+
+  // function to search for movies
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}${title}`);
     const data = await response.json();
 
     setMovies(data.Search);
   };
+
+  // useEffect hook to fetch movies on component mount
   useEffect(() => {
     const fetchMovies = async () => {
+      // create an array of promises to fetch movie data for each title
       const promises = movieTitles.map((title) =>
         fetch(`${API_URL}${title}`).then((response) => response.json())
       );
+
+      // wait for all promises to resolve and combine movie data
       const results = await Promise.all(promises);
       const movies = results.reduce((acc, result) => {
         if (result.Search) {
@@ -40,8 +55,11 @@ const APP = () => {
         }
         return acc;
       }, []);
+
+      // shuffle the movie data and update state
       setMovies(shuffleArray(movies));
     };
+
     fetchMovies();
   }, []);
 
@@ -65,6 +83,7 @@ const APP = () => {
         </div>
         {movies?.length > 0 ? (
           <div className="container">
+            {/* For each movie in the movies array, render a MovieCard component */}
             {movies.map((movie) => {
               return <MovieCard movie={movie} />;
             })}
@@ -75,6 +94,7 @@ const APP = () => {
           </div>
         )}
       </div>
+      {/* Any other components or elements that need to be rendered can go here */}
     </>
   );
 };
